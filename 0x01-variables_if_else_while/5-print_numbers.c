08fbcb194a069eb9e245d8fb924a904e209eb6bc
TariroYashe <rukodzac@gmail.com>
2023-06-16 22:10:18 -0700

    programme that print one to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints 1 to 9
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+index i;
+
+for (index = 0; index < 10; index++)
+{
+putchar(index);
+}
+putchar('\n);
+return (0);
+}
