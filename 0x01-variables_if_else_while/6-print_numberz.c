f71bb29739b52482cb494c0a089848a307f9c885
TariroYashe <rukodzac@gmail.com>
2023-06-16 23:45:49 -0700

    programme that print one to ten in a newline

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: programme that print 0 to 10 in a newline
+ *
+ * Return: always (0)
+ */
+int main(void)
+
+{
+int index=0;
+
+while (index < 10)
+
+{putchar(index);
+index++
+}
+
+putchar('\n');
+
+return (0);
+
+}
