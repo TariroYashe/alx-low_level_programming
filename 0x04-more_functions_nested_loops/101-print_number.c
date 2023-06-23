88ab562b99fe241be8f5623359a1a84fb53c37c7
TariroYashe <rukodzac@gmail.com>
2023-06-23 08:05:12 -0700

    function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_number - Prints an integr
+* @n: the integer ti print
+* Return: 0
+*/
+void print_number(int n)
+{
+int divisor = 1;
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+while (n / divisor >= 10)
+divisor *= 10;
+
+while (divisor != 0)
+{
+_putchar('0' + n / divisor);
+n %= divisor;
+divisor /= 10;
+}
+}
