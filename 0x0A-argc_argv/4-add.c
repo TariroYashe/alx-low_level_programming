543c73f52e36c8dbb0d07573ecd315708a9e1958
TariroYashe <rukodzac@gmail.com>
2023-07-12 23:00:45 -0700

    programmethat add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers.
- * @argc: arg count
- * @argv: arr of args
- *
- * Description: "adds positive numbers."
- *
- * Return: Always 0 (success) otherwise 1 (error)
- *
- */
+* main - adds positive numbers.
+* @argc: arg count
+* @argv: arr of args
+*
+* Description: "adds positive numbers."
+*
+* Return: Always 0 (success) otherwise 1 (error)
+*
+*/
 
 int main(int argc, char *argv[])
 {
-	long int total = 0;
-	int idx;
-	int num;
+long int total = 0;
+int idx;
+int num;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
 
-	for (idx = 1; idx < argc; idx++)
-	{
-		num = atoi(argv[idx]);
+for (idx = 1; idx < argc; idx++)
+{
+num = atoi(argv[idx]);
 
-		if (num <= 0 && argv[1][0] != '0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		total += num;
-	}
-	printf("%ld\n", total);
-	return (0);
+if (num <= 0 && argv[1][0] != '0')
+{
+printf("Error\n");
+return (1);
+}
+total += num;
+}
+printf("%ld\n", total);
+return (0);
 }
