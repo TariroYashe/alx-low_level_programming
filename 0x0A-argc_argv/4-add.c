5eee7905010ddf3d9c354d91b8f5d0caf7134fd1
TariroYashe <rukodzac@gmail.com>
2023-07-12 22:34:59 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,55 +1,40 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
-* check_num - check string there are digit
-* @str: array str
-* Return: 0 (success)
-*/
-
-int check_num(char *str)
-{
-unsigned int count = 0;
-
-while (count < strlen(str))
-{
-if (!isdigit(str[count]))
-{
-return (0);
-}
-count++;
-}
-return (1);
-}
-
-/**
-* main - print the name of the programme
-* @argc: count the arguments
-* @argv: arguments
-* Return: 0 (success)
-*/
+ * main - adds positive numbers.
+ * @argc: arg count
+ * @argv: arr of args
+ *
+ * Description: "adds positive numbers."
+ *
+ * Return: Always 0 (success) otherwise 1 (error)
+ *
+ */
 
 int main(int argc, char *argv[])
 {
-int count;
-int str_to_int;
-int sum = 0;
+	long int total = 0;
+	int idx;
+	int num;
 
-count = 1;
-while (count < argc)
-{
-if (check_num(argv[count]))
-sum += str_to_int;
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-count++;
-}
-printf("%d\n", sum);
-return (0);
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (idx = 1; idx < argc; idx++)
+	{
+		num = atoi(argv[idx]);
+
+		if (num <= 0 && argv[1][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		total += num;
+	}
+	printf("%ld\n", total);
+	return (0);
 }
